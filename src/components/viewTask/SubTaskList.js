b4ebf15633c57ac26dd52760182acc8a94c33857
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-08-30 01:16:58 +0530

    Add view task component

---
+++ src/components/viewTask/SubTaskList.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Checkbox from "../form/checkbox/Checkbox";
+import { SubTaskListPane, SubTaskListItem, SubTaskBody } from "./styles";
+
+export default function SubTaskList({ dataSource }) {
+  const [subTaskList, setSubTaskList] = useState(dataSource);
+
+  const subTaskClickHandler = (id) => {
+    setSubTaskList((prev) => {
+      const newState = [...prev];
+      prev.forEach((item) => {
+        if (item.id === id) item.isComplete = !item.isComplete;
+      });
+      return newState;
+    });
+  };
+
+  return (
+    <SubTaskListPane>
+      {subTaskList.map((item) => {
+        return (
+          <SubTaskListItem
+            key={item.id}
+            onClick={subTaskClickHandler.bind(null, item.id)}
+          >
+            <Checkbox state={item.isComplete} />
+            <SubTaskBody state={item.isComplete}>{item.body}</SubTaskBody>
+          </SubTaskListItem>
+        );
+      })}
+    </SubTaskListPane>
+  );
+}
---
+++ src/components/viewTask/styles.js
@@ -0,0 +1,67 @@
+import styled from "@emotion/styled/macro";
+
+export const ViewTaskPane = styled.section`
+  border-radius: 8px;
+  color: ${(props) => [props.theme.colors.textPrimary]};
+  margin: 1em;
+  max-width: 720px;
+  display: flex;
+  align-items: center;
+  min-height: 80vh;
+
+  @media (min-width: ${(props) => props.theme.media.medium}) {
+    margin: 0 auto;
+    max-width: 500px;
+  }
+`;
+
+export const TaskTitle = styled.h3``;
+
+export const TaskDescription = styled.p`
+  color: ${(props) => [props.theme.colors.textSecondary]};
+  font-size: 0.8rem;
+  line-height: 1.75em;
+  font-weight: 700;
+`;
+
+export const SubTaskTitle = styled.h4`
+  font-size: 0.9rem;
+`;
+
+export const StatusTitle = styled.h4`
+  font-size: 0.9rem;
+`;
+
+export const SubTaskListPane = styled.ul`
+  list-style: none;
+  padding: 0;
+  font-size: 0.8rem;
+`;
+
+export const SubTaskListItem = styled.li`
+  background-color: ${(props) => [props.theme.colors.background]};
+  padding: 0.25em 1em;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+
+  & + & {
+    margin-top: 0.5em;
+  }
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${(props) => [props.theme.colors.backgroundLight]};
+  }
+`;
+
+export const SubTaskBody = styled.p`
+  margin-left: 1em;
+  line-height: 1.25em;
+  font-weight: 700;
+
+  color: ${({ state, theme }) =>
+    state ? theme.colors.textSecondary : theme.colors.textPrimary};
+
+  text-decoration: ${({ state }) => (state ? "line-through" : "")};
+`;
