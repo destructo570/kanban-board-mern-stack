e31f7d61b6840ead1189e87a8f66dd1b0e1bcc53
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-09-08 00:39:40 +0530

    Add logic to add new task to database

--- src/containers/AddTaskContainer.js
+++ src/containers/AddTaskContainer.js
@@ -13,13 +13,41 @@ import FormInput from "../components/form/input/FormInput";
 import Button from "../components/form/button/Button";
 import FormTextArea from "../components/form/textArea/FormTextArea";
 import BackDrop from "../components/common/backDrop/BackDrop";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewCard } from "../store/board-slice";
 
 export default function AddTaskContainer({ onClose }) {
+  const activeBoard = useSelector((state) => state.board.activeBoard);
+  const [titleInput, setTitleInput] = useState("");
   const [subTaskList, setSubTaskList] = useState([""]);
+  const [descInput, setDescInput] = useState("");
+  const [statusInput, setStatusInput] = useState("");
+  const dispatch = useDispatch();
+  const statusChangeHandler = (status) => {
+    setStatusInput(status);
+  };
+  const titleChangeHandler = (e) => setTitleInput(e.target.value);
+  const descChangeHandler = (e) => setDescInput(e.target.value);
 
-  const statusChangeHandler = () => {};
+  const createTaskHandler = () => {
+    console.log(activeBoard);
+    const list = activeBoard.lists.find(
+      (item) => item.title.toLowerCase() === statusInput.toLowerCase()
+    );
+    const payload = {
+      boardId: activeBoard._id,
+      listId: list._id,
+      title: titleInput,
+      description: descInput,
+      checkList: subTaskList.map((item) => ({
+        body: item,
+        isCompleted: false,
+      })),
+    };
 
-  const createTaskHandler = () => {};
+    dispatch(createNewCard(payload));
+    onClose();
+  };
   return (
     <>
       {ReactDOM.createPortal(
@@ -42,6 +70,8 @@ export default function AddTaskContainer({ onClose }) {
               type="text"
               placeholder="e.g. Take coffee break"
               width="100%"
+              value={titleInput}
+              onChange={titleChangeHandler}
             />
             <FormTextArea
               label="Description"
@@ -49,6 +79,8 @@ export default function AddTaskContainer({ onClose }) {
               placeholder="e.g. It's always good to take a break. This 15 minute break will recharge the batteries a little"
               width="100%"
               minHeight="150px"
+              value={descInput}
+              onChange={descChangeHandler}
             />
             <SubTaskTitle>Subtasks</SubTaskTitle>
             <AddSubTaskList
