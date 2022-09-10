3d9f7b33a42f3e78c19b2bd8323de7ac6304c53e
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-09-10 13:40:05 +0530

    Add hook to detect click outside of an element

---
+++ src/hooks/useClickOutside.js
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from "react";
+
+export default function useClickOutside(handler) {
+  let domNode = useRef();
+  useEffect(() => {
+    let maybeHandler = (event) => {
+      if (!domNode.current.contains(event.target)) {
+        handler();
+      }
+    };
+
+    document.addEventListener("mousedown", maybeHandler);
+    return () => {
+      document.removeEventListener("mousedown", maybeHandler);
+    };
+  });
+  return domNode;
+}
